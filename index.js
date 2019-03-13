b07e2aaf88ab573a9c29bad24cba68c0e70121ed
mazenswar <mazenswar@protonmail.com>
2019-03-13 15:16:12 -0400

    string repitition

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+const heyJude = () => "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+
+
+let regExp = /candycorn|brusselssprouts/
+const prohibitedLanguage = (str) => str.search(regExp);
+
+
+
+
+//Create a function called prohibitedLanguage that will prevent usernames with the Strings "candycorn" and/or "brusselssprouts"
